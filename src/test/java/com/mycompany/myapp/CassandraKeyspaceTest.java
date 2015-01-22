6de0835df9252d73051ea71e8427c8d904964595
Julien Dubois <julien.dubois@gmail.com>
2015-01-23 00:13:55 +0100

    First test passes

--- src/test/java/com/mycompany/myapp/CassandraKeyspaceTest.java
+++ src/test/java/com/mycompany/myapp/CassandraKeyspaceTest.java
@@ -2,6 +2,7 @@ package com.mycompany.myapp;
 
 import com.datastax.driver.core.ResultSet;
 import com.mycompany.myapp.config.Constants;
+import com.mycompany.myapp.config.apidoc.SwaggerConfiguration;
 import org.cassandraunit.CassandraCQLUnit;
 import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
 import org.junit.ClassRule;
@@ -9,11 +10,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.inject.Inject;
 
@@ -21,6 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
 public class CassandraKeyspaceTest {
 
     protected final Logger log = LoggerFactory.getLogger(this.getClass().getCanonicalName());
